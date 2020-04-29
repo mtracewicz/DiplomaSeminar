cb4664be59797c59588c5c7f886595515b1c9d50
Michał Tracewicz <m.tracewicz@gmail.com>
2020-04-29 16:40:10 +0200

    Images display when uploaded, tf model loads

--- 006_NeuralNetworks/MNIST_Web/worker.js
+++ 006_NeuralNetworks/MNIST_Web/worker.js
@@ -1,9 +1,19 @@
 importScripts(
-    "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@1.2.7/dist/tf.min.js"
+  "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@1.2.7/dist/tf.min.js"
 );
-tf.setBackend("cpu");
-const model = await tf.loadLayersModel("conv.json");
-input = tf.tensor(seq);
-predictOut = model.predict(input);
-predictOut.dispose();
-console.log(model);
+
+onmessage = async function predict(e) {
+  tf.setBackend("cpu");
+  const input = preprocessImage(e.data)
+  const model = await tf.loadLayersModel("model.json");
+  predictOut = model.predict(input);
+  this.postMessage(predictOut);
+  predictOut.dispose();
+};
+
+function preprocessImage(image) {
+   return tf.tensor(image)
+    // .reshape([28,28,1])
+    // .resizeNearestNeighbor([28, 28])
+    .toFloat();
+}
