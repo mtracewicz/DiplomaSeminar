7787a829e8c00ed539264a1085975f9b3323dcd8
mtracewicz <m.tracewicz@gmail.com>
2020-05-05 18:39:22 +0200

    Changed data loading to properly laod images

--- 006_NeuralNetworks/MNIST_Web/worker.js
+++ 006_NeuralNetworks/MNIST_Web/worker.js
@@ -5,15 +5,21 @@ importScripts(
 onmessage = async function predict(e) {
   tf.setBackend("cpu");
   const input = preprocessImage(e.data);
-  console.log(await input.data())
   const model = await tf.loadLayersModel("model/model.json");
   const predictionArray = await model.predict(input).data();
   postMessage(getPredictionFromArray(predictionArray));
 };
 
 function preprocessImage(image) {
-  return tf.browser
-    .fromPixels({ data: image, width: 28, height: 28 }, 1)
+  let data = new Float32Array(image);
+  data = data.map(i => i/255.0);
+  let processed_data = new Float32Array(data.length/4);
+  for(let i = 0;i<processed_data.length;i++){
+    if(data[4*i] != 0 || data[1+4*i] != 0 || data[2+4*i] != 0){
+      processed_data[i] = 1;
+    }
+  }
+  return tf.tensor(processed_data)
     .asType("float32")
     .reshape([1, 28, 28, 1]);
 }
