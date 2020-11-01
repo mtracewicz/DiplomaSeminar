e351edefd059d9902d3018c0967b620b08e68bf1
Michał Tracewicz <m.tracewicz@gmail.com>
2020-11-01 19:45:30 +0100

    Test

--- 008_U-Net/unet.py
+++ 008_U-Net/unet.py
@@ -5,95 +5,66 @@ import tensorflow as tf
 import numpy as np
 from PIL import Image
 
-def create_model(learning_rate):
-    # Model creation
-    model = tf.keras.models.Sequential()
-    # Downsampling
-    model.add(tf.keras.layers.Conv2D(
-        filters=16,
-        kernel_size=3,
-        padding='same',
-        activation='relu',
-        use_bias=True,
-        input_shape=(1200, 1600, 3)
-    ))
-    model.add(tf.keras.layers.MaxPooling2D(pool_size=2))
-    model.add(tf.keras.layers.Dropout(rate=0.2))
+def conv2d_block(input_tensor, n_filters, kernel_size=3, batchnorm=True):
+    # first layer
+    x = Conv2D(filters=n_filters, kernel_size=(kernel_size, kernel_size), kernel_initializer="he_normal",
+               padding="same")(input_tensor)
+    if batchnorm:
+        x = BatchNormalization()(x)
+    x = Activation("relu")(x)
+    # second layer
+    x = Conv2D(filters=n_filters, kernel_size=(kernel_size, kernel_size), kernel_initializer="he_normal",
+               padding="same")(x)
+    if batchnorm:
+        x = BatchNormalization()(x)
+    x = Activation("relu")(x)
+    return x
 
-    model.add(tf.keras.layers.Conv2D(
-        filters=32,
-        kernel_size=3,
-        use_bias=True,
-        padding='same',
-        activation='relu'
-    ))
-    model.add(tf.keras.layers.MaxPooling2D(pool_size=2))
-    model.add(tf.keras.layers.Dropout(rate=0.2))
 
-    model.add(tf.keras.layers.Conv2D(
-        filters=64,
-        kernel_size=3,
-        use_bias=True,
-        padding='same',
-        activation='relu'
-    ))
-    model.add(tf.keras.layers.MaxPooling2D(pool_size=2))
-    model.add(tf.keras.layers.Dropout(rate=0.2))
+def create_model(input_img,n_filters=16, dropout=0.5, batchnorm=True):
+    # contracting path
+    c1 = conv2d_block(input_img, n_filters=n_filters*1, kernel_size=3, batchnorm=batchnorm)
+    p1 = MaxPooling2D((2, 2)) (c1)
+    p1 = Dropout(dropout*0.5)(p1)
 
-    #Flat
-    model.add(tf.keras.layers.Conv2D(
-        filters=128,
-        kernel_size=3,
-        use_bias=True,
-        padding='same',
-        activation='relu'
-    ))
+    c2 = conv2d_block(p1, n_filters=n_filters*2, kernel_size=3, batchnorm=batchnorm)
+    p2 = MaxPooling2D((2, 2)) (c2)
+    p2 = Dropout(dropout)(p2)
 
-    # Upsampling
-    model.add(tf.keras.layers.UpSampling2D())
-    model.add(tf.keras.layers.Conv2D(
-        filters=64,
-        kernel_size=3,
-        use_bias=True,
-        padding='same',
-        activation='relu'
-    ))
-    model.add(tf.keras.layers.Dropout(rate=0.2))
+    c3 = conv2d_block(p2, n_filters=n_filters*4, kernel_size=3, batchnorm=batchnorm)
+    p3 = MaxPooling2D((2, 2)) (c3)
+    p3 = Dropout(dropout)(p3)
 
-    model.add(tf.keras.layers.UpSampling2D())
-    model.add(tf.keras.layers.Conv2D(
-        filters=64,
-        kernel_size=3,
-        use_bias=True,
-        padding='same',
-        activation='relu'
-    ))
-    model.add(tf.keras.layers.Dropout(rate=0.2))
+    c4 = conv2d_block(p3, n_filters=n_filters*8, kernel_size=3, batchnorm=batchnorm)
+    p4 = MaxPooling2D(pool_size=(2, 2)) (c4)
+    p4 = Dropout(dropout)(p4)
+    
+    c5 = conv2d_block(p4, n_filters=n_filters*16, kernel_size=3, batchnorm=batchnorm)
+    
+    # expansive path
+    u6 = Conv2DTranspose(n_filters*8, (3, 3), strides=(2, 2), padding='same') (c5)
+    u6 = concatenate([u6, c4])
+    u6 = Dropout(dropout)(u6)
+    c6 = conv2d_block(u6, n_filters=n_filters*8, kernel_size=3, batchnorm=batchnorm)
 
-    model.add(tf.keras.layers.UpSampling2D())
-    model.add(tf.keras.layers.Conv2D(
-        filters=64,
-        kernel_size=3,
-        use_bias=True,
-        padding='same',
-        activation='relu'
-    ))
-    model.add(tf.keras.layers.Dropout(rate=0.2))
+    u7 = Conv2DTranspose(n_filters*4, (3, 3), strides=(2, 2), padding='same') (c6)
+    u7 = concatenate([u7, c3])
+    u7 = Dropout(dropout)(u7)
+    c7 = conv2d_block(u7, n_filters=n_filters*4, kernel_size=3, batchnorm=batchnorm)
 
-    # Flatenning
-    model.add(tf.keras.layers.Conv2D(
-        filters=16,
-        kernel_size=3,
-        use_bias=True,
-        padding='same',
-        activation='relu'
-    ))
+    u8 = Conv2DTranspose(n_filters*2, (3, 3), strides=(2, 2), padding='same') (c7)
+    u8 = concatenate([u8, c2])
+    u8 = Dropout(dropout)(u8)
+    c8 = conv2d_block(u8, n_filters=n_filters*2, kernel_size=3, batchnorm=batchnorm)
 
-    model.compile(
-        optimizer=tf.keras.optimizers.Adam(lr=learning_rate),
-        loss="sparse_categorical_crossentropy",
-        metrics=['accuracy']
-    )
+    u9 = Conv2DTranspose(n_filters*1, (3, 3), strides=(2, 2), padding='same') (c8)
+    u9 = concatenate([u9, c1], axis=3)
+    u9 = Dropout(dropout)(u9)
+    c9 = conv2d_block(u9, n_filters=n_filters*1, kernel_size=3, batchnorm=batchnorm)
+    
+    outputs = Conv2D(1, (1, 1), activation='sigmoid') (c9)
+    model = Model(inputs=[input_img], outputs=[outputs])
+    model.compile(optimizer=Adam(), loss="binary_crossentropy", metrics=["accuracy"])
     return model
 
 
@@ -127,15 +98,14 @@ if __name__ == "__main__":
     # and reshaping it for proper dimensions
     x_train_normalized = (x_train/255.0).reshape(x_train.shape[0], 1200, 1600, 3)
     # Establish the model's topography.
-    model = create_model(learning_rate)
+    model = create_model(x_train)
     # Train the model on the normalized training set.
     model.fit(
         x=x_train_normalized,
         y=y_train,
         batch_size=batch_size,
         epochs=epochs,
-        shuffle=True,
-        validation_split=validation_split
+        shuffle=True
     )
     res = Image(model.predict(x_train_normalized[0]))
     res.save("res.jpeg")
