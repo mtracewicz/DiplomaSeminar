78ba0a88091d4de50fd0758a5de0b584cb1eff95
Michał Tracewicz <m.tracewicz@gmail.com>
2020-11-08 21:29:28 +0100

    Various improvments

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 pip install -r /DiplomaSeminar/requirements.txt 
-groupadd -g $G mtracewicz
-useradd -u $U -g $G -ms /bin/bash mtracewicz
+groupadd -g $U mtracewicz
+useradd -u $U -g $U -ms /bin/bash mtracewicz
 chown -R mtracewicz:mtracewicz /DiplomaSeminar
--- start_docker_with_tf.sh
+++ start_docker_with_tf.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --gpus all -it --rm -e U=$UID -e G=$GID --name U-NET -v /home/mtracewicz/DiplomaSeminar/:/DiplomaSeminar tensorflow/tensorflow:latest-gpu bash
+docker run --gpus all -it --rm -e U=$UID --name U-NET -v /home/mtracewicz/DiplomaSeminar/:/DiplomaSeminar tensorflow/tensorflow:latest-gpu bash -c -c "/DiplomaSeminar/init.sh;su mtracewicz"
