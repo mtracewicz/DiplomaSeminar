edbfe1170d96a3ee26e05095507291ee0d53325b
Michał Tracewicz <m.tracewicz@gmail.com>
2020-11-21 20:54:25 +0100

    Added sorting to processing displaying results

--- 009_ImageManipulation/imagesmainpulator.py
+++ 009_ImageManipulation/imagesmainpulator.py
@@ -4,5 +4,6 @@ from imagespliter import ImageSplitter
 if __name__ == "__main__":
     splitter = ImageSplitter()
     images = os.listdir("Img")
+    images.sort()
     for img in images:
         splitter.split_into_images(img)
--- 009_ImageManipulation/imagespliter.py
+++ 009_ImageManipulation/imagespliter.py
@@ -7,21 +7,20 @@ class ImageSplitter():
 
     def __init__(self):
         self._image_width = 1600
-        self._image_height = 1300
-        self._new_image_size = 100
-        self._horizontal_pocket = 25
-        self._vertical_pocket = 20
+        self._image_height = 1200
+        self._new_image_size = 90
+        self._pocket = 10
 
     def split_into_images(self, image_path):
         self._make_sure_directory_exists()
         img = np.array(Image.open(f'Img/{image_path}'))
-        number_of_moves_horizontally = int(self._image_width/(self._new_image_size-self._horizontal_pocket))
-        number_of_moves_vertically = int(self._image_height/(self._new_image_size-self._vertical_pocket))
+        number_of_moves_horizontally = int(self._image_width/(self._new_image_size-self._pocket))
+        number_of_moves_vertically = int(self._image_height/(self._new_image_size-self._pocket))
         for i in range(number_of_moves_horizontally):
             for j in range(number_of_moves_vertically):
-                horizontal_start = i*(self._new_image_size - self._horizontal_pocket)
-                vertical_start = j*(self._new_image_size - self._vertical_pocket)
-                tmp_img = Image.fromarray(np.copy(img[horizontal_start:horizontal_start+self._new_image_size,vertical_start:vertical_start+self._new_image_size]))
+                horizontal_start = i*(self._new_image_size - self._pocket)
+                vertical_start = j*(self._new_image_size - self._pocket)
+                tmp_img = Image.fromarray(np.copy(img[vertical_start:vertical_start+self._new_image_size,horizontal_start:horizontal_start+self._new_image_size]))
                 tmp_img.save(f"split/{ImageSplitter.it}_{'0' if i < 10 else ''}{i}_{'0' if j < 10 else ''}{j}_tmp.jpg")
         ImageSplitter.it+=1
 
