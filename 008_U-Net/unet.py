206f385bdb09d13fc705c0913ba463a109a4a870
Michał Tracewicz <m.tracewicz@gmail.com>
2020-12-03 11:09:46 +0100

    Changed reading of input/output data

--- 008_U-Net/unet.py
+++ 008_U-Net/unet.py
@@ -14,19 +14,27 @@ if __name__ == "__main__":
         exit()
 
     #Loading data
-    IMAGES_DIRECTORY= os.getcwd()+'/008_U-Net/Img'
+    IMAGES_DIRECTORY= os.getcwd()+'/008_U-Net/_in'
     filenames = os.listdir(IMAGES_DIRECTORY)
     number_of_images = len(filenames)
-    x_train = np.ones((number_of_images,1200,1600,3))
+    x_train = np.ones((number_of_images,1200,1600,4))
     for i,filename in enumerate(filenames):
         img = Image.open(f"{IMAGES_DIRECTORY}/{filename}")
         x_train[i] = np.array(img)
 
+    IMAGES_DIRECTORY= os.getcwd()+'/008_U-Net/_out'
+    filenames = os.listdir(IMAGES_DIRECTORY)
+    number_of_images = len(filenames)
+    y_train = np.ones((number_of_images,1200,1600,4))
+    for i,filename in enumerate(filenames):
+        img = Image.open(f"{IMAGES_DIRECTORY}/{filename}")
+        y_train[i] = np.array(img)
+
     # Data normalization from [0,255] to [0.0,1.0]
     # and reshaping it for proper dimensions
-    x_train_normalized = (x_train/255.0).reshape(x_train.shape[0], 1200, 1600, 3)
+    x_train_normalized = (x_train/255.0).reshape(x_train.shape[0], 1200, 1600, 4)
     # Prepare grand truth
-    y_train = ((255.0-np.copy(x_train)/255.0).reshape(x_train.shape[0], 1200, 1600, 3))
+    y_train_normalized = (y_train/255.0).reshape(y_train.shape[0], 1200, 1600, 4))
 
     tf.keras.backend.set_floatx('float64')
     # Establish the model's topography.
@@ -35,6 +43,6 @@ if __name__ == "__main__":
 
     # Save model
     save_model(model,sys.argv[1])
-    res = Image.fromarray(((model.predict(np.array(Image.open(f'{os.getcwd()}/008_U-Net/Img/przyp. 1 STAT 3 pow 40x.JPG.JPG'))*255))).astype(np.uint8))
+    res = Image.fromarray(((model.predict(np.array(Image.open(f'{os.getcwd()}/008_U-Net/)_in/0.png'))*255))).astype(np.uint8))
     res.save("res.jpeg")
     print('Finished!')
